b8b001d92d4345960e6d4e299cf1bf7ab5422203
kogp112 <gapkana@gmail.com>
2020-04-10 23:12:08 +0900

    modify main.go for fetching static data

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -2,17 +2,43 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
+    "encoding/json"
     "log"
     "net/http"
     "github.com/gorilla/mux"
 )
 
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
+type event struct {
+  ID  string `json:"ID"`
+  Name  string `json:"Title"`
+  Age  int `json:"Age"`
+}
+
+type fetchEventsAll []event
+
+var events = fetchEventsAll{
+  {
+    ID: "1",
+    Name: "Kana",
+    Age: 30,
+  },
+}
+
+func fetch(w http.ResponseWriter, r *http.Request) {
+  var newEvent event
+  reqBody, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    fmt.Fprintf(w, "error happend")
+  }
+  json.Unmarshal(reqBody, &newEvent)
+  events = append(events, newEvent)
+  w.WriteHeader(http.StatusCreated)
+  json.NewEncoder(w).Encode(newEvent)
 }
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
-	log.Fatal(http.ListenAndServe(":8081", router))
+  router := mux.NewRouter().StrictSlash(true)
+  router.HandleFunc("/", fetch)
+  log.Fatal(http.ListenAndServe(":8081", router))
 }
