2c58a49725f56600c64699930acc4881446bef10
kanami.ogawa <kanami.ogawa@rakuten.com>
2020-04-10 22:52:01 +0900

    create simple api return text

---
+++ go-rest-api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8081", router))
+}
--- src/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-  fmt.Println("Hello let's go api")
-}
