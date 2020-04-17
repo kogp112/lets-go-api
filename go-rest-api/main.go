7252c392ea8d1d53a205c16530feca3d3d509ff4
kogp112 <gapkana@gmail.com>
2020-04-17 23:15:03 +0900

    add post method

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -2,48 +2,60 @@ package main
 
 import (
 	"encoding/json"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 // struct is a collection fields
-type event struct {
+type Person struct {
 	ID   string `json:"ID"`
 	Name string `json:"Title"`
 	Age  int    `json:"Age"`
 }
 
 // slice doesn't have size, it's dinamically flexible and common
-type fetchEventsAll []event
+var persons []Person
 
-var events = fetchEventsAll{
-	{
-		ID:   "1",
-		Name: "Kana",
-		Age:  30,
-	},
+func getPersons(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(persons)
 }
 
-func getEvents(w http.ResponseWriter, r *http.Request) {
+func getPerson(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for _, item := range events {
+	for _, item := range persons {
 		if item.ID == params["userID"] {
 			json.NewEncoder(w).Encode(item)
 			break
 		}
 		return
 	}
-	json.NewEncoder(w).Encode(&event{})
+	json.NewEncoder(w).Encode(&Person{})
+}
+
+func createPersons(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var data Person
+	_ = json.NewDecoder(r.Body).Decode(&data)
+	data.ID = strconv.Itoa(rand.Intn(1000000))
+	persons = append(persons, data)
+	json.NewEncoder(w).Encode(&data)
 }
 
 func main() {
 	// initialize new router
 	router := mux.NewRouter().StrictSlash(true)
+	// create mock datas
+	persons = append(persons, Person{ID: "1", Name: "Kanako", Age: 30})
+	persons = append(persons, Person{ID: "2", Name: "Taro", Age: 33})
 	// create api endpoints
-	router.HandleFunc("/events", getEvents).Methods(http.MethodGet)
-	router.HandleFunc("/events/{userID}", getEvents).Methods(http.MethodGet)
+	router.HandleFunc("/events", getPersons).Methods(http.MethodGet)
+	router.HandleFunc("/events/{userID}", getPerson).Methods(http.MethodGet)
+	router.HandleFunc("/events", createPersons).Methods(http.MethodPost)
 
 	http.ListenAndServe(":8081", router)
 }
